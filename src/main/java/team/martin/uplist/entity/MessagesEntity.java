c617771a53248224d5f4d632e33b059da4d703b7
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-23 01:27:57 -0300

    ⚙️Inclusão do PrePersist para não entregar valores nulos.

--- src/main/java/team/martin/uplist/entity/MessagesEntity.java
+++ src/main/java/team/martin/uplist/entity/MessagesEntity.java
@@ -1,6 +1,8 @@
 package team.martin.uplist.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -27,4 +29,9 @@ public class MessagesEntity {
     @Column
     private Long total_ups;
 
+    @PrePersist
+    void preInsert() {
+        if (this.total_ups == null)
+            this.total_ups = 0L;
+    }
 }
