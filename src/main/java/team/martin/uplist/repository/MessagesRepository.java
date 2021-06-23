560236a82b828f9754294bd9cc319b42f4252b7a
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-23 19:17:44 -0300

    ⚙️ Criação de filtro de mensagens

--- src/main/java/team/martin/uplist/repository/MessagesRepository.java
+++ src/main/java/team/martin/uplist/repository/MessagesRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team.martin.uplist.entity.MessagesEntity;
 
+import java.util.List;
+
 @Repository
 public interface MessagesRepository extends JpaRepository<MessagesEntity, Long> {
+
+    List<MessagesEntity> findByOrderByReactionsDesc();
 }
