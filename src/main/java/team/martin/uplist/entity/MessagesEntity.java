eb3276a8f2ba71186cb9ff0bda863fc6d5d53786
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-25 21:12:27 -0300

    ⚙ Ajustes pós testes de integração

--- src/main/java/team/martin/uplist/entity/MessagesEntity.java
+++ src/main/java/team/martin/uplist/entity/MessagesEntity.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -16,11 +18,20 @@ import javax.persistence.*;
 @EqualsAndHashCode
 public class MessagesEntity {
 
+    public MessagesEntity(String message) {
+        this.message = message;
+    }
+
+    public MessagesEntity() {
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
+    @NotEmpty(message = "O campo não pode estar vazio.")
+    @Size(max = 200, message = "A mensagem não pode passar de 200 caracteres.")
     private String message;
 
     @Column
