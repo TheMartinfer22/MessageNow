d7f5ff5aed42eb79903215c3f90c4965b65bc345
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-25 21:12:09 -0300

    ⚙ Documentação

--- src/main/java/team/martin/uplist/controller/MessagesController.java
+++ src/main/java/team/martin/uplist/controller/MessagesController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import team.martin.uplist.entity.MessagesEntity;
 import team.martin.uplist.service.MessagesService;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -19,25 +20,35 @@ public class MessagesController {
 
     // MENSAGEM
 
-    /*
+    /**
      * Quando a mensagem ser passada no método POST em JSON,
      * irá apontar para a classe que facilitará a edição de novas regras de negócio.
+     *
+     * @param message Recebe a mensagem no formato String.
+     * @return MessagesEntity - retorna a entidade criada em JSON com Long Id, String message, long reactions.
      */
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public MessagesEntity createMessage(@RequestBody MessagesEntity message) throws Exception {
+    public MessagesEntity createMessage(@RequestBody @NotEmpty MessagesEntity message) {
         return messagesService.createMessage(message);
     }
 
-    /*
+    /**
      * Quando o método GET for executado irá retornar a lista de mensagens que foram adicionadas
      * ao banco de dados.
+     *
+     * @return List<MessagesEntity> - retorna TODAS entidades do banco de dados com Long Id, String message, long reactions.
      */
     @GetMapping
     List<MessagesEntity> listMessages(){
         return messagesService.listMessages();
     }
 
+    /**
+     * Deleta a mensagem pelo endpoint ao fornecer a ID da mesma.
+     *
+     * @param id Long - Irá deletar a mensagem ao ser passada a identicação da mesma.
+     */
     @DeleteMapping("/{id}/delete")
     public void removeMessage(@PathVariable Long id){
         messagesService.removeMessage(id);
@@ -45,8 +56,10 @@ public class MessagesController {
 
     // REACTIONS
 
-    /*
-     * Irá adicionar um voto na mensagem quando for passado o path com a ID da mesma.
+    /**
+     * Irá adicionar uma reação na mensagem quando for acessado o endpoint com a ID da mesma.
+     *
+     * @param id Long - Adiciona uma reação a mensagem.
      */
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @GetMapping(value = "/reaction/{id}/add")
@@ -54,8 +67,10 @@ public class MessagesController {
         messagesService.addReaction(id);
     }
 
-    /*
-     * Irá remover um voto na mensagem quando for passado o path com a ID da mesma.
+    /**
+     * Irá remover uma reação na mensagem quando for acessado o endpoint com a ID da mesma.
+     *
+     * @param id Long - Remove uma reação da mensagem.
      */
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @GetMapping(value = "/reaction/{id}/remove")
