172e2d18355c4f680529136df9a9969fffe36eac
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-26 01:25:55 -0300

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ import "./css/styles.css"
 const Title = () => {
     return (
         <div>
-            <p className="title not_selected"><strong>UpList</strong> - Instant Messages </p>
+            <p className="title not_selected"><strong>MessageNow</strong> - Instant Messages </p>
             <p className="description not_selected">Envie suas mensagens liveremente e com limite de <strong>200</strong> caracteres.</p>
         </div>
     )
